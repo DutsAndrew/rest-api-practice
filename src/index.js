c48058dfb3fac7f0f2aaa6121602ec4e3df08805
Andrew Dutson <Dutsandrew@gmail.com>
2023-03-20 15:02:28 -0600

    api practice completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+dotenv.config();
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
 
 app.get('/users', (req, res) => {
-  return res.send("Received a GET HTTP method");
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.get('/session', (req, res) => {
+  return res.send(users[req.me.id]);
 });
 
 app.post('/users', (req, res) => {
   return res.send("Received a POST HTTP method");
+});
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+  };
+  messages[id] = message;
+  return res.send(message);
 });
 
 app.put('/users/:userId', (req, res) => {
@@ -24,6 +84,14 @@ app.delete('/users/:userId', (req, res) => {
   return res.send(
     `DELETE HTTP method on user/${req.params.userId} resource`,
   );
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = messages;
+  return res.send(message);
 });
 
 app.listen(process.env.PORT, () => {
