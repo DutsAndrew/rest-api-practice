e605aa4570ac13ac23ea797b4bb37131f11ef52d
Andrew Dutson <Dutsandrew@gmail.com>
2023-03-19 20:55:59 -0600

    base file upload

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/users', (req, res) => {
+  return res.send("Received a GET HTTP method");
+});
+
+app.post('/users', (req, res) => {
+  return res.send("Received a POST HTTP method");
+});
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}!`);
+});
